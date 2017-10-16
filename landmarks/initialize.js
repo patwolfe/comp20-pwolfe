69a597591a4d1591731fba7049ca725b76d17d18
wolfep15 <patrick.wolfe@tufts.edu>
2017-10-16 13:12:06 -0400

    successfully displayed other users on map

--- landmarks/initialize.js
+++ landmarks/initialize.js
@@ -11,12 +11,15 @@
     var map;
 	var marker;
 	var infowindow = new google.maps.InfoWindow();
+	var people;
+	var landmarks;
+	var markers = [];
+
 
 	function init()
 	{
 		console.log("init map");
 		map = new google.maps.Map(document.getElementById("map"), options); 
-        console.log(map)
 		getLocation();
 	}
 
@@ -36,21 +39,58 @@
 	}
 
 	function renderMap(){
-		loc = new google.maps.LatLng(lat, lng);
+				loc = new google.maps.LatLng(lat, lng);
 
-        map.panTo(loc);
+		       
 
-        console.log("rendering map with location" + loc);
-        console.log(map);
+		        console.log("rendering map with location" + loc);
+		        console.log(map);
+				marker = new google.maps.Marker({
+						position: loc,
+						title: "Your Location"
+				});
+				marker.setMap(map);
+				google.maps.event.addListener(marker, 'click', function() {
+						infowindow.setContent(marker.title);
+						infowindow.open(map, marker);
+				})
 
-		marker = new google.maps.Marker({
-			position: loc,
-			title: "Your Location"
-		});
-		marker.setMap(map);
+		request.open("POST", "https://defense-in-derpth.herokuapp.com/sendLocation", true);
+		userData = "login=O6VNCD83&lat=" + lat + "&lng=" + lng;
+		request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.setContent(marker.title);
-			infowindow.open(map, marker);
-		})
+		request.onreadystatechange = function() {
+			if (request.readyState == 4 && request.status == 200) {
+				//if request is successful, save parsed json data in arrays 
+				rawData = request.responseText;
+				data = JSON.parse(rawData);
+				people = data.people;
+				console.log(data.people);
+				landmarks = data.landmarks;
+				console.log(people);
+				console.log(landmarks);
+
+
+		        for(i = 0; i < people.length; i++) {
+		        	if(people[i].lat != lat && people[i].lng != lng){
+			        	markers[i] = {};
+						markers[i] = new google.maps.Marker({
+							position: new google.maps.LatLng(people[i].lat,people[i].lng),
+							title: people[i].login
+						});
+						markers[i].setMap(map);
+						google.maps.event.addListener(markers[i], 'click', function() {
+						infowindow.setContent(markers[i].title);
+						infowindow.open(map, markers[i]);
+				})
+					}
+				}
+			}
+		}
+
+
+		request.send(userData); 
+
+ 		map.panTo(loc);
+
 	}
